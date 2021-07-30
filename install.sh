f5e9a7e7923eae3d89ac0dd60ad9e1a8646542bb
Vish <vish@pop-os.localdomain>
2021-07-30 14:43:04 +0530

    build(app): added a better installation script and upgraded all deps

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+echo "Upgrading auth service.."
+cd auth 
+npx npm-check --update-all
+npm i
+cd ..
+
+echo "Upgrading tickets microservice.."
+cd tickets 
+npx npm-check --update-all
+npm i
+cd ..
+
+echo "Upgrading orders microservice.."
+cd orders 
+npx npm-check --update-all
+npm i
+cd ..
+
+echo "Installing client dependencies.."
+cd client 
+npm i
+cd ..
+
+echo "Upgrading orders microservice.."
+cd ticketing-common 
+npx npm-check --update-all
+npm i
+cd ..
+
+echo "Upgrading nats-test packages..." 
+cd nats-test
+npx npm-check --update-all
+npm i 
+cd ..
+
+echo "Upgrading common library..." 
+cd ticketing-common 
+npx npm-check --update-all
+npm i 
+cd ..
