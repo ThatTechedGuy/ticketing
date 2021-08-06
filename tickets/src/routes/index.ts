be5c44e8e51921b0f86a73935a23f1cac55b31d6
ThatTechedGuy <thattechedguy@gmail.com>
2021-08-07 01:26:34 +0530

    fix(tickets): tickets service should not return bought or reserved tickets

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -4,7 +4,7 @@ import { Ticket } from "../models/ticket";
 const router = express.Router();
 
 router.get("/api/tickets", async (_, res: Response) => {
-  const tickets = await Ticket.find();
+  const tickets = await Ticket.find({ orderId: undefined });
   res.status(201).send(tickets);
 });
 
